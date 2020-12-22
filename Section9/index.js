302896ad4655f0f17969a2b16fdcb87067119cf4
YosukeShibazaki <tebasaki4839@gmail.com>
2020-12-22 20:34:10 +0900

    sec9-118

--- Section9/index.js
+++ Section9/index.js
@@ -2,11 +2,16 @@
 class Score {
 }
 class Food {
+    constructor(element) {
+        this.element = element;
+    }
 }
 class Foods {
     constructor() {
         this.elements = document.querySelectorAll('.food'); // foodクラスを全て取得する。
         this.elements.forEach(element => {
+            new Food(element);
         });
     }
 }
+const foods = new Foods();
--- Section9/index.ts
+++ Section9/index.ts
@@ -3,14 +3,19 @@ class Score {
 }
 
 class Food {
+    constructor(public element: HTMLDivElement){
 
+    }
 }
 
 class Foods {
-    elements = document.querySelectorAll('.food'); // foodクラスを全て取得する。
-    constructor(){
-        this.elements.forEach( element => {
+    elements = document.querySelectorAll<HTMLDivElement>('.food'); // foodクラスを全て取得する。
 
+    constructor(){
+        this.elements.forEach( element => { // foodクラスの要素全てを1つずつインスタンス生成する。
+            new Food(element);
         })
     }
 }
+
+const foods = new Foods();
