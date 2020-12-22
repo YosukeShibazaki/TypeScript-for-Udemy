5890d1d61b3f5086d3f2374ee4d4b15d743c93b6
YosukeShibazaki <tebasaki4839@gmail.com>
2020-12-22 20:48:40 +0900

    sec9-120

--- Section9/index.js
+++ Section9/index.js
@@ -4,10 +4,11 @@ class Score {
 class Food {
     constructor(element) {
         this.element = element;
-        element.addEventListener('click', this.clickEventHandler);
+        element.addEventListener('click', this.clickEventHandler.bind(this)); // ここのthisはFoodクラスのこと。
     }
     clickEventHandler() {
         // foodクラスの要素に、food--activeクラスがあれば、food--activeを消す。なければ付ける。
+        // 呼び出しもとでbind(this)を書くと、この関数内のthisはFoodクラス(bindで指定したthis)となる。
         this.element.classList.toggle('food--active');
     }
 }
--- Section9/index.ts
+++ Section9/index.ts
@@ -4,11 +4,12 @@ class Score {
 
 class Food {
     constructor(public element: HTMLDivElement){
-        element.addEventListener('click', this.clickEventHandler);
+        element.addEventListener('click', this.clickEventHandler.bind(this)); // ここのthisはFoodクラスのこと。
     }
 
     clickEventHandler(){
         // foodクラスの要素に、food--activeクラスがあれば、food--activeを消す。なければ付ける。
+        // 呼び出しもとでbind(this)を書くと、この関数内のthisはFoodクラス(bindで指定したthis)となる。
         this.element.classList.toggle('food--active');
     }
 }
