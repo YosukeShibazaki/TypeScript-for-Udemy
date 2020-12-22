ca518b3091d0d25f284795bf20e9ea8af72c3ded
YosukeShibazaki <tebasaki4839@gmail.com>
2020-12-23 07:23:41 +0900

    sec9-125

--- Section9/index.js
+++ Section9/index.js
@@ -1,7 +1,8 @@
 "use strict";
 class Score {
+    constructor() { } // 何もしない
     get totalScore() {
-        const foods = new Foods();
+        const foods = Foods.getInstance();
         // 合計を返す
         // reducdeの第1引数は、コールバック関数。第2引数は合計したい値の初期値。scoreには配列の値が代入される。
         return foods.activeElementsScore.reduce((total, score) => total + score, 0);
@@ -9,6 +10,14 @@ class Score {
     render() {
         // 合計点をレンダリングする
         document.querySelector('.score__number').textContent = String(this.totalScore);
+    }
+    static getInstance() {
+        // シングルトンパターンの使用。
+        if (!Score.instance) {
+            // インスタンスが生成されていない場合のみインスタンスを生成するようにする。
+            Score.instance = new Score();
+        }
+        return Score.instance;
     }
 }
 class Food {
@@ -20,7 +29,7 @@ class Food {
         // foodクラスの要素に、food--activeクラスがあれば、food--activeを消す。なければ付ける。
         // 呼び出しもとでbind(this)を書くと、この関数内のthisはFoodクラス(bindで指定したthis)となる。
         this.element.classList.toggle('food--active');
-        const score = new Score();
+        const score = Score.getInstance();
         score.render();
     }
 }
@@ -57,5 +66,13 @@ class Foods {
         });
         return this._activeElementsScore;
     }
+    static getInstance() {
+        // シングルトンパターンの使用。
+        if (!Foods.instance) {
+            // インスタンスが生成されていない場合のみインスタンスを生成するようにする。
+            this.instance = new Foods();
+        }
+        return Foods.instance;
+    }
 }
-const foods = new Foods();
+const foods = Foods.getInstance();
--- Section9/index.ts
+++ Section9/index.ts
@@ -1,6 +1,10 @@
 class Score {
+    private static instance: Score;
+
+    private constructor(){} // 何もしない
+
     get totalScore(){
-        const foods = new Foods();
+        const foods = Foods.getInstance();
         // 合計を返す
         // reducdeの第1引数は、コールバック関数。第2引数は合計したい値の初期値。scoreには配列の値が代入される。
         return foods.activeElementsScore.reduce((total, score) => total + score, 0);
@@ -9,6 +13,15 @@ class Score {
     render(){
         // 合計点をレンダリングする
         document.querySelector('.score__number')!.textContent = String(this.totalScore);
+    }
+
+    static getInstance(){
+        // シングルトンパターンの使用。
+        if(!Score.instance){
+            // インスタンスが生成されていない場合のみインスタンスを生成するようにする。
+            Score.instance = new Score();
+        }
+        return Score.instance;
     }
 }
 
@@ -21,12 +34,13 @@ class Food {
         // foodクラスの要素に、food--activeクラスがあれば、food--activeを消す。なければ付ける。
         // 呼び出しもとでbind(this)を書くと、この関数内のthisはFoodクラス(bindで指定したthis)となる。
         this.element.classList.toggle('food--active');
-        const score = new Score();
+        const score = Score.getInstance();
         score.render();
     }
 }
 
 class Foods {
+    private static instance: Foods;
     elements = document.querySelectorAll<HTMLDivElement>('.food'); // foodクラスを全て取得する。
     private _activeElements: HTMLDivElement[] = []; // food--activeクラスを格納するためのプロパティ。
     private _activeElementsScore: number[] = []; // food--activeクラスが付いた要素の、food__scoreのテキストを格納する配列。
@@ -57,11 +71,20 @@ class Foods {
         return this._activeElementsScore;
     }
 
-    constructor(){
+    private constructor(){
         this.elements.forEach( element => { // foodクラスの要素全てを1つずつインスタンス生成する。
             new Food(element);
         })
     }
+
+    static getInstance(){
+        // シングルトンパターンの使用。
+        if(!Foods.instance){
+            // インスタンスが生成されていない場合のみインスタンスを生成するようにする。
+            this.instance = new Foods();
+        }
+        return Foods.instance;
+    }
 }
 
-const foods = new Foods();
+const foods = Foods.getInstance();
