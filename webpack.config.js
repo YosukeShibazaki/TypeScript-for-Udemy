1b93bf653457e1bdbf098898dd203aa7107fe54a
YosukeShibazaki <tebasaki4839@gmail.com>
2020-12-23 21:09:18 +0900

    sec10-136

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,10 @@
+const path = require(__dirname); // __dirnameはこのファイルがあるディレクトの絶対パスを取得する。
+console.log(path.resolve(__dirname, 'bandle.index'));
+
 module.exports = {
-    entry: "./Section9・10/main.js"
+    entry: './Section9・10/main.js',　// どのファイルからbandleしていくか。
+    output: { // bandle先についての設定
+        filename: 'bandle.js', // bandle後のファイルの名前
+        path: path.resolve(__dirname, 'bandle.index') // バンドルしたファイルをどこに作成するか
+    }
 }
